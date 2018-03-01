a07b4150b035230b5a465d80fb06193daee6327b
熊本熊 <apple@hongdingdeMac-mini.local>
2018-03-01 14:29:32 +0800

    完成

--- RongChatRoomDemo/Main/首页/ViewController/HomeViewController.m
+++ RongChatRoomDemo/Main/首页/ViewController/HomeViewController.m
@@ -68,6 +68,7 @@
 - (void)viewDidLoad {
    
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view.
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.view.backgroundColor =RGB(220, 220, 220);
